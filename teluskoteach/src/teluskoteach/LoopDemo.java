a4e3f4d9493ca6f06ed32306458cb929c8191edf
Suresh M S <Suresh M S@LAPTOP-CI4COSF6>
2019-06-26 16:56:51 +0530

    Commit this

---
+++ teluskoteach/src/teluskoteach/LoopDemo.java
@@ -0,0 +1,37 @@
+package teluskoteach;
+
+public class LoopDemo {
+
+	public static void main(String[] args) 
+	{
+		int i=1;
+		while(i<=5)
+		{
+			System.out.println("Hello");
+			i+=1;
+		}
+		int b=1;
+		do
+		{
+			System.out.println("hai"); 
+			b+=1;
+			
+		}while(b<=5);
+		
+		for(int a=1;a<=5;a++){
+			System.out.println("heyyy");
+		}
+		for(char  j=65;j<=71;j++)
+		{
+			for(char c=65;c<=j;c++){
+				System.out.print(c +"");
+			}
+			System.out.println();
+			
+		}
+		
+		
+
+	}
+
+}
